03199b229d3f7e71cd6ebf5e294457bb8e54a225
Glitch (glitch-hello-website) <none>
2023-12-31 18:31:47 +0000

    🐂🍏 Checkpoint ./public/script.js:59499274/28336 ./public/index.html:59499274/2367 ./package.json:59499274/31

--- public/script.js
+++ public/script.js
@@ -31,64 +31,197 @@ if (btn) { // Detect clicks on the button
 
 // Function to fetch data from ThingSpeak
 async function fetchData() {
-    const channelID = '2369829'; // Replace with your ThingSpeak channel ID
-    const apiKey = '75U1406FS2G9BFG8'; // Replace with your ThingSpeak API key
+  const channelID = "2369829"; // Replace with your ThingSpeak channel ID
+  const apiKey = "75U1406FS2G9BFG8"; // Replace with your ThingSpeak API key
 
-    const urlField1 = `https://api.thingspeak.com/channels/${channelID}/fields/1.json?api_key=${apiKey}&results=10`;
-    const urlField2 = `https://api.thingspeak.com/channels/${channelID}/fields/2.json?api_key=${apiKey}&results=10`;
+  const urlField1 = `https://api.thingspeak.com/channels/${channelID}/fields/1.json?api_key=${apiKey}&results=10`;
+  const urlField4 = `https://api.thingspeak.com/channels/${channelID}/fields/4.json?api_key=${apiKey}&results=10`;
 
-    try {
-        const responseField1 = await fetch(urlField1);
-        const dataField1 = await responseField1.json();
+  const urlHumidityField2 = `https://api.thingspeak.com/channels/${channelID}/fields/2.json?api_key=${apiKey}&results=10`;
+  const urlHumidityField5 = `https://api.thingspeak.com/channels/${channelID}/fields/5.json?api_key=${apiKey}&results=10`;
 
-        const responseField2 = await fetch(urlField2);
-        const dataField2 = await responseField2.json();
+  const urlPressureField3 = `https://api.thingspeak.com/channels/${channelID}/fields/3.json?api_key=${apiKey}&results=10`;
+  const urlPressureField6 = `https://api.thingspeak.com/channels/${channelID}/fields/6.json?api_key=${apiKey}&results=10`;
 
-        // Process dataField1 and dataField2 to create datasets for the chart
-        const labels = dataField1.feeds.map(feed => feed.created_at);
-        const valuesField1 = dataField1.feeds.map(feed => feed.field1);
-        const valuesField2 = dataField2.feeds.map(feed => feed.field2);
+  try {
+    const responseField1 = await fetch(urlField1);
+    const dataField1 = await responseField1.json();
 
-        // Create a chart using Chart.js
-        const ctx = document.getElementById('myChart').getContext('2d');
-        const myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [
-                    {
-                        label: 'Field 1',
-                        data: valuesField1,
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        borderWidth: 1,
-                        fill: false,
-                    },
-                    {
-                        label: 'Field 2',
-                        data: valuesField2,
-                        borderColor: 'rgba(255, 99, 132, 1)',
-                        borderWidth: 1,
-                        fill: false,
-                    },
-                ],
+    const responseField4 = await fetch(urlField4);
+    const dataField4 = await responseField4.json();
+
+    // Extract timestamp and values for both fields
+    const timestamps = dataField1.feeds.map((feed) => moment(feed.created_at));
+    const valuesField1 = dataField1.feeds.map((feed) => feed.field1);
+    const valuesField4 = dataField4.feeds.map((feed) => feed.field4);
+
+    const responseHumidityField2 = await fetch(urlHumidityField2);
+    const dataHumidityField2 = await responseHumidityField2.json();
+
+    const responseHumidityField5 = await fetch(urlHumidityField5);
+    const dataHumidityField5 = await responseHumidityField5.json();
+
+    // Extract timestamp and values for both humidity fields
+    const timestampsHumidity = dataHumidityField2.feeds.map((feed) =>
+      moment(feed.created_at)
+    );
+    const valuesHumidityField2 = dataHumidityField2.feeds.map(
+      (feed) => feed.field2
+    );
+    const valuesHumidityField5 = dataHumidityField5.feeds.map(
+      (feed) => feed.field5
+    );
+
+    const responsePressureField3 = await fetch(urlPressureField3);
+    const dataPressureField3 = await responsePressureField3.json();
+
+    const responsePressureField6 = await fetch(urlPressureField6);
+    const dataPressureField6 = await responsePressureField6.json();
+
+    // Extract timestamp and values for both pressure fields
+    const timestampsPressure = dataPressureField3.feeds.map((feed) =>
+      moment(feed.created_at)
+    );
+    const valuesPressureField3 = dataPressureField3.feeds.map(
+      (feed) => feed.field3
+    );
+    const valuesPressureField6 = dataPressureField6.feeds.map(
+      (feed) => feed.field6
+    );
+
+    // Create a chart using Chart.js
+    const ctxTemp = document.getElementById("tempChart").getContext("2d");
+    const tempChart = new Chart(ctxTemp, {
+      type: "line",
+      data: {
+        labels: timestamps.map((ts) => ts.format("HH:mm:ss")),
+        datasets: [
+          {
+            label: "Indoor Temperature",
+            data: valuesField1,
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderWidth: 1,
+            fill: false,
+          },
+          {
+            label: "Outdoor Temperature",
+            data: valuesField4,
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+            fill: false,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          x: {
+            type: "category",
+          },
+        },
+        plugins: {
+          title: {
+            display: true,
+            text: "Today's Temperature",
+            font: {
+              size: 16,
             },
-            options: {
-                scales: {
-                    x: {
-                        type: 'linear',
-                        position: 'bottom',
-                    },
-                },
+          },
+        },
+      },
+    });
+
+    // Create a humidity chart using Chart.js
+    const ctxHumidity = document
+      .getElementById("humidityChart")
+      .getContext("2d");
+    const humidityChart = new Chart(ctxHumidity, {
+      type: "line",
+      data: {
+        labels: timestampsHumidity.map((ts) => ts.format("HH:mm:ss")),
+        datasets: [
+          {
+            label: "Indoor Humidity",
+            data: valuesHumidityField2,
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderWidth: 1,
+            fill: false,
+          },
+          {
+            label: "Outdoor Humidity",
+            data: valuesHumidityField5,
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+            fill: false,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          x: {
+            type: "category",
+          },
+        },
+        plugins: {
+          title: {
+            display: true,
+            text: "Today's Humidity",
+            font: {
+              size: 16,
             },
-        });
-    } catch (error) {
-        console.error('Error fetching data from ThingSpeak:', error);
-    }
+          },
+        },
+      },
+    });
+
+    // Create a pressure chart using Chart.js
+    const ctxPressure = document
+      .getElementById("pressureChart")
+      .getContext("2d");
+    const pressureChart = new Chart(ctxPressure, {
+      type: "line",
+      data: {
+        labels: timestampsPressure.map((ts) => ts.format("HH:mm:ss")),
+        datasets: [
+          {
+            label: "Indoor Pressure",
+            data: valuesPressureField3,
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderWidth: 1,
+            fill: false,
+          },
+          {
+            label: "Outdoor Pressure",
+            data: valuesPressureField6,
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+            fill: false,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          x: {
+            type: "category",
+          },
+        },
+        plugins: {
+          title: {
+            display: true,
+            text: "Today's Pressure",
+            font: {
+              size: 16,
+            },
+          },
+        },
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching data from ThingSpeak:", error);
+  }
 }
 
 // Call the fetchData function when the page loads
-document.addEventListener('DOMContentLoaded', fetchData);
-
+document.addEventListener("DOMContentLoaded", fetchData);
 
 // ----- GLITCH STARTER PROJECT HELPER CODE -----
 
